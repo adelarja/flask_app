b2ebbe79acd44d12142c230cecbd6f0072e9397c
Adel Arja <adelarja@gmail.com>
2021-10-05 21:14:26 -0300

    Added login and registration forms

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_bootstrap import Bootstrap
 
 # local imports
 from config import app_config
@@ -23,6 +24,8 @@ def create_app(config_name):
     login_manager.login_view = "auth.login"
 
     migrate = Migrate(app, db)
+
+    Bootstrap(app)
 
     from app import models
 
